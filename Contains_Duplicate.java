f5e0151e3acff86aa94a37eca75d28f473431986
Pranav Sinha <sinhapranav23@gmail.com>
2026-02-23 22:04:55 +0530

    Implement ContainsDuplicate method with HashSet
    This method utilizes a HashSet to efficiently determine if any duplicates exist in the input array.

---
+++ Contains_Duplicate.java
@@ -0,0 +1,15 @@
+//leetcode question contains Duplicate
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+       HashSet<Integer> seen = new HashSet<>();
+       for (int i =0 ; i< nums.length ;i++){
+        if (seen.contains(nums[i])){
+            return true ;
+
+        }
+        seen.add(nums[i]);
+       }
+       return false ;
+    }
+}
